6815b6059134267b140cf03f3e507e194929c262
luizssilva99 <luizssilva_@outlook.com>
2024-08-09 14:01:58 -0300

    Adicionar .bat para rodar o script

---
+++ executar_script.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Defina o caminho para o Python, se não estiver no PATH do sistema
+set PYTHON_PATH="C:\Users\Luiz Fernando\AppData\Local\Programs\Python\Python311\python.exe"
+
+REM Defina o caminho para o script Python
+set SCRIPT_PATH="C:\Users\Luiz Fernando\Desktop\day-price-tracking\main.py"
+
+REM Executa o script Python
+python %SCRIPT_PATH%
+
+REM Pause para manter a janela aberta após a execução
+pause
