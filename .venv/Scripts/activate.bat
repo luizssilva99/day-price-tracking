c566c669afbad2503c096e9454e5ee7082171f4b
luizssilva99 <luizssilva_@outlook.com>
2024-08-09 12:31:25 -0300

    Atualização de codigo

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\Luiz Fernando\Desktop\Monitoramento de Preço Diário\.venv
+set VIRTUAL_ENV=c:\Users\Luiz Fernando\Desktop\day-price-tracking\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
+import sqlite3
 from time import sleep
 from datetime import datetime
 from tabulate import tabulate
@@ -13,8 +15,15 @@ class WebPageAccess:
         self.driver = None
 
     def setup_driver(self):
+        # Configurando o ChromeDriver em modo headless
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
         # Configurando o ChromeDriver usando WebDriver Manager
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     def open_page(self):
         if self.driver is None:
@@ -30,6 +39,29 @@ class WebPageAccess:
     def close_driver(self):
         if self.driver is not None:
             self.driver.quit()
+
+def create_database():
+    conn = sqlite3.connect('precos.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS precos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            valor_antigo TEXT,
+            tipo_pagamento TEXT,
+            valor_vista TEXT,
+            desconto_vista TEXT,
+            valor_parcelado TEXT,
+            qnt_parcelas TEXT,
+            link TEXT,
+            data_hora_execucao TEXT
+        )
+    ''')
+    conn.commit()
+    return conn
+
+def insert_data(conn, df):
+    df.to_sql('precos', conn, if_exists='append', index=False)
 
 if __name__ == "__main__":
     url = "https://www.pichau.com.br/hardware/memorias"  # Substitua pela URL que deseja acessar
@@ -47,7 +79,7 @@ if __name__ == "__main__":
         # Processamento dos dados extraídos
         text = tabela.text
         linhas = text.split('\n')
-        print(linhas)
+        # print(linhas)
 
         nome = []
         valor_antigo = []
@@ -82,22 +114,25 @@ if __name__ == "__main__":
                 i += 1
 
         df = pd.DataFrame({
-            'Nome': nome,
-            'Valor Antigo': valor_antigo,
-            'Tipo de Pagamento': tipo_pag_vsta,
-            'Valor a Vista': valor_vista,
-            'Desconto a Vista': desconto_vista,
-            'Valor Parcelado': valor_parc,
-            'Quantidade de Parcelas': qnt_parc,
-            'Link': links,  # Nova coluna com os links
-            'Data e Hora de Execução': data_hora_execucao
+            'nome': nome,
+            'valor_antigo': valor_antigo,
+            'tipo_pagamento': tipo_pag_vsta,
+            'valor_vista': valor_vista,
+            'desconto_vista': desconto_vista,
+            'valor_parcelado': valor_parc,
+            'qnt_parcelas': qnt_parc,
+            'link': links,  # Nova coluna com os links
+            'data_hora_execucao': data_hora_execucao
         })
+        
+        # Criar e conectar ao banco de dados SQLite
+        conn = create_database()
 
-        # Exibição do DataFrame formatado
-        print(tabulate(df, headers='keys', tablefmt='pretty'))   
+        # Inserir os dados no banco de dados
+        insert_data(conn, df)
 
-        df.to_excel(f'tabela_precos{datetime.now().strftime('%Y-%m-%d')}.xlsx', index=False)       
-
+        # Fechar a conexão
+        conn.close()
 
     except Exception as e:
         print(f"Elemento não encontrado: {e}")
